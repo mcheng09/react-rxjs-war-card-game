8b338fc65da61ac9d37059e80f77ab71a7894565
Michael <mcheng.oc@gmail.com>
2020-01-06 23:00:09 -0800

    feat(App): adds basic observers from rxjs documentation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import { Observable } from 'rxjs';
 
 import './App.css'
+
+const observable = new Observable(subscriber => {
+  subscriber.next(1);
+  subscriber.next(2);
+  subscriber.next(3);
+  setTimeout(() => {
+    subscriber.next(4);
+    subscriber.complete();
+  }, 1000);
+});
+
+console.log('just before subscribe');
+observable.subscribe({
+  next(x) { console.log('got value ' + x); },
+  error(err) { console.error('something wrong occurred: ' + err); },
+  complete() { console.log('done'); }
+});
+console.log('just after subscribe');
 
 function App() {
 
@@ -73,6 +92,7 @@ function App() {
 
   return (
     <div className='App'>
+
       <p>What is your name?</p>
       <input value={playerName} onChange={(e) => setPlayerName(e.target.value)}></input>
       <div className='scoreboard'>
